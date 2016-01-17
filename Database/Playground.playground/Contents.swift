4f6102eecc4f0558ec582be9d30f8627106f9879
Orlando Cossia Castiglioni <occ.github@hairesis.com>
2016-01-17 16:34:15 +0100

    Add validation stuff

--- Database/Playground.playground/Contents.swift
+++ Database/Playground.playground/Contents.swift
@@ -1,9 +1,24 @@
 import UIKit
 
+// Data
 
+let names = ["John", "Adam"]
+let weights = [65, 75]
 
+// Validation
 
+precondition(names.count == weights.count)
 
+for weight in weights {
+    precondition(weight > 0)
+}
 
+// Output
 
+let indexes = 0...1
 
+for index in indexes {
+    let name = names[index]
+    let weight = weights[index]
+    print(name, weight)
+}
